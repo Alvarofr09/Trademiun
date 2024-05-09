e0497030bb82d28df864d6388a8524b6874c3484
PabloFC <pablofc_68@hotmail.com>
2024-05-09 18:19:40 +0200

    mejorando notificaciones

--- client/src/components/WebNotificaciones.jsx
+++ client/src/components/WebNotificaciones.jsx
@@ -1,7 +1,7 @@
 const WebNotificaciones = ({ nombre, notificacion, foto }) => {
   return (
     <div className="flex justify-start ml-12 2xl:ml-28  ">
-      <div className="flex bg-fondoWebApp p-4 max-w-xs rounded-lg mb-2">
+      <div className="flex bg-fondoWebApp p-2 max-w-xs rounded-lg mb-2">
         <div className="basis-1/4 self-center">
           <img
             className="inline-block h-12 w-12 rounded-full ring-2 ring-transparent  "
@@ -9,7 +9,7 @@ const WebNotificaciones = ({ nombre, notificacion, foto }) => {
             alt="avatar"
           />
         </div>
-        <div className="basis-3/4">
+        <div className="basis-3/4 mr-3">
           <h6 className="font-bold">{nombre}</h6>
           <p>{notificacion}</p>
         </div>
