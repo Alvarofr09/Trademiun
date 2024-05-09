07a675b4af2483e7742899dce73c4dc1e3e14ff3
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-09 19:26:10 +0200

    Traer los initial values

---
+++ client/src/consts/InitialValues.js
@@ -0,0 +1,29 @@
+export const RegisterFormInitialValues = {
+	username: "",
+	email: "",
+	confirmEmail: "",
+	password: "",
+	confirmPassword: "",
+	acceptedTC: false,
+};
+
+export const LoginFormInitialValues = {
+	email: "",
+	password: "",
+};
+
+export const CreateGroupFormInitialValues = {
+	groupName: "",
+	description: "",
+	price: 0,
+};
+
+export const SignalFormInitialValues = {
+	signalImage: "",
+	description: "",
+	coin: "",
+	entrada: "",
+	salida: "",
+	tp: "",
+	porcentaje: "",
+};
