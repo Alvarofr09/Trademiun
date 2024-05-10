b6166e9bb8397ab2b848c102a62cf523ba427e34
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-10 18:45:09 +0200

    Logica de que si es mensaje o señal

--- client/src/components/Chat/Messages.jsx
+++ client/src/components/Chat/Messages.jsx
@@ -4,15 +4,52 @@ export default function Messages({ messages, scrollRef }) {
 	return (
 		<div className="chat-messages bg-white scrollbar-custom py-4 px-8 flex flex-col gap-4 overflow-auto">
 			{messages.map((message) => {
+				console.log(message);
 				return (
 					<div ref={scrollRef} key={uuidv4()}>
 						<div
-							className={`message  flex items-center ${
+							className={`  flex items-center ${
 								message.fromSelf ? "sended" : "recieved"
-							}`}
+							} ${message.type === "signal" ? "signal" : "mensaje"}`}
 						>
-							<div className="content max-w-[60%] break-all p-4 text-lg rounded-2xl text-[#000000]">
-								{message.message}
+							<div
+								className={`content max-w-[60%] break-all p-4 text-lg rounded-2xl  ${
+									message.type === "signal"
+										? "text-[#ffffff]"
+										: "text-[#000000]"
+								}`}
+							>
+								{message.type === "message" ? (
+									message.message
+								) : (
+									<>
+										<p>
+											<strong>Descripcion: </strong> {message.description}
+										</p>
+										<p>
+											<strong>Moneda: </strong> {message.moneda}
+										</p>
+										<p>
+											<strong>Riesgo: </strong> {message.riesgo}%
+										</p>
+										<p>
+											<strong>Entrada: </strong> {message.entrada}
+										</p>
+										<p>
+											<strong>SL: </strong> {message.stopLoss}
+										</p>
+										<p>
+											<strong>TP: </strong> {message.takeProfit}
+										</p>
+										<p
+											className={`titulo ${
+												message.isCompra ? "text-emerald-600" : "text-red-600"
+											} `}
+										>
+											{message.isCompra ? "COMPRA" : "VENTA"}
+										</p>
+									</>
+								)}
 							</div>
 						</div>
 					</div>
