ecb1c24c72ee591f80b6eceaa0af381e9d810857
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-10 18:47:29 +0200

    Quitar ruta

--- server/routers/signalsRoutes.js
+++ server/routers/signalsRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { addSignal } = require("../controllers/signalController");
+const { addSignal, getSignals } = require("../controllers/signalController");
 
 const signalRoute = express.Router();
 
 signalRoute.post("/add-signal", addSignal);
 
-// signalRoute.post("/getGroupMessages", getGroupMessages);
+signalRoute.post("/get-signals", getSignals);
 
 module.exports = signalRoute;
