11047215e13501560adea82055b97d98a3deb6bc
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-05-10 12:48:33 +0200

    Comnetar chatProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,14 +5,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
 import AuthContextProvider from "./context/AuthContext";
+// import ChatContextProvider from "./context/ChatContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<AuthContextProvider>
+				{/* <ChatContextProvider> */}
 				<Routes>
 					<Route path="/*" element={<App />} />
 				</Routes>
+				{/* </ChatContextProvider> */}
 			</AuthContextProvider>
 		</BrowserRouter>
 	</React.StrictMode>
