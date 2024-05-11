6abcbf2e1d628bc18ad3d35801ea1d6ccfc11f15
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-05-11 13:21:03 +0200

    Usar cosa

--- server/routers/signalsRoutes.js
+++ server/routers/signalsRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { addSignal, getSignals } = require("../controllers/signalController");
+const upload = require("../utils/multerConfig");
 
 const signalRoute = express.Router();
 
-signalRoute.post("/add-signal", addSignal);
+signalRoute.post("/add-signal", upload.single("image"), addSignal);
 
 signalRoute.post("/get-signals", getSignals);
 
