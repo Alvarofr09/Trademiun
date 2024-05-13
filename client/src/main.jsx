3581f6220ad94288a3d267863b79700df83697bb
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-13 16:38:28 +0200

    UserContextProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,17 +5,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
 import AuthContextProvider from "./context/AuthContext";
+import UserContextProvider from "./context/UserContext.jsx";
 // import ChatContextProvider from "./context/ChatContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<AuthContextProvider>
-				{/* <ChatContextProvider> */}
-				<Routes>
-					<Route path="/*" element={<App />} />
-				</Routes>
-				{/* </ChatContextProvider> */}
+				<UserContextProvider>
+					{/* <ChatContextProvider> */}
+					<Routes>
+						<Route path="/*" element={<App />} />
+					</Routes>
+					{/* </ChatContextProvider> */}
+				</UserContextProvider>
 			</AuthContextProvider>
 		</BrowserRouter>
 	</React.StrictMode>
