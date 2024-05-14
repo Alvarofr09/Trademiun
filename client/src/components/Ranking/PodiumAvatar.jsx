760b4101d40e2cff7c541c37ab2d9f82c8df4ffc
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-14 18:15:06 +0200

    cambiar tamaños

--- client/src/components/Ranking/PodiumAvatar.jsx
+++ client/src/components/Ranking/PodiumAvatar.jsx
@@ -2,11 +2,13 @@ export default function PodiumAvatar({ image, ranking }) {
 	return (
 		<div className="relative flex justify-center">
 			<img
-				className="inline-block h-24 w-24 rounded-full filter brightness-50 contrast-150"
+				className={`inline-block h-36 w-36 rounded-full filter brightness-50 contrast-150 ${
+					ranking === 1 ? "-mb-16" : "-mb-14"
+				}`}
 				src={image}
 				alt="avatar"
 			/>
-			<h1 className="absolute text-white inset-0 text-3xl text-center ">
+			<h1 className="absolute text-white font-bold pt-10 inset-0 text-3xl text-center ">
 				{ranking}
 			</h1>
 		</div>
