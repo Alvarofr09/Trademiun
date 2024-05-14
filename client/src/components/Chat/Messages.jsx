7c42df9df4c9f52be98a4cbed1042832ac04de3c
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-05-14 13:56:04 +0200

    Usar el componente Signal

--- client/src/components/Chat/Messages.jsx
+++ client/src/components/Chat/Messages.jsx
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
+import Signal from "../ui/Signal";
 
 export default function Messages({ messages, scrollRef }) {
 	return (
@@ -7,53 +8,19 @@ export default function Messages({ messages, scrollRef }) {
 				console.log(message);
 				return (
 					<div ref={scrollRef} key={uuidv4()}>
-						<div
-							className={`  flex items-center ${
-								message.fromSelf ? "sended" : "recieved"
-							} ${message.type === "signal" ? "signal" : "mensaje"}`}
-						>
+						{message.type === "message" ? (
 							<div
-								className={`content max-w-[60%] break-normal text-lg rounded-2xl  ${
-									message.type === "signal"
-										? "text-[#ffffff] py-5 px-10"
-										: "text-[#000000] p-4 "
+								className={`flex items-center mensaje ${
+									message.fromSelf ? "sended" : "recieved"
 								}`}
 							>
-								{message.type === "message" ? (
-									message.message
-								) : (
-									<>
-										{message.image && (
-											<img src={message.image} className="w-full" alt="" />
-										)}
-										{message.description && <p>{message.description}</p>}
-
-										<p>
-											<strong>Moneda: </strong> {message.moneda}
-										</p>
-										<p>
-											<strong>Riesgo: </strong> {message.riesgo}%
-										</p>
-										<p>
-											<strong>Entrada: </strong> {message.entrada}
-										</p>
-										<p>
-											<strong>SL: </strong> {message.stopLoss}
-										</p>
-										<p>
-											<strong>TP: </strong> {message.takeProfit}
-										</p>
-										<p
-											className={`titulo ${
-												message.isCompra ? "text-emerald-600" : "text-red-600"
-											} `}
-										>
-											{message.isCompra ? "COMPRA" : "VENTA"}
-										</p>
-									</>
-								)}
+								<div className="content max-w-[60%] break-normal text-lg rounded-2xl text-[#000000] p-4">
+									{message.message}
+								</div>
 							</div>
-						</div>
+						) : (
+							<Signal signal={message} />
+						)}
 					</div>
 				);
 			})}
