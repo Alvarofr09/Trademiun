3d5b5498d114bae0c78834aa1dd6e85de227a2af
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-16 00:02:33 +0200

    Hacer que al clickar la img salga un modal

--- client/src/components/ui/CloudinaryImg.jsx
+++ client/src/components/ui/CloudinaryImg.jsx
@@ -1,5 +1,7 @@
 import { AdvancedImage, lazyload, placeholder } from "@cloudinary/react";
 import { Cloudinary } from "@cloudinary/url-gen";
+import { useState } from "react";
+import Modal from "../Modal";
 
 // Fill te pone la imagen entera en el tamaño de la caja
 // Thumbnail redimensiona la imagen
@@ -10,21 +12,41 @@ import { Cloudinary } from "@cloudinary/url-gen";
 // import { FocusOn } from "@cloudinary/url-gen/qualifiers/focusOn";
 
 export default function Img({ uploadedImg, className }) {
+	const [showModal, setShowModal] = useState(false);
 	const cld = new Cloudinary({
 		cloud: {
 			cloudName: "dzfjiypm9",
 		},
 	});
 
+	const showImage = () => {
+		setShowModal(true); // Mostrar el modal al activar la función
+	};
+
+	const closeModal = () => {
+		setShowModal(false); // Cerrar el modal
+	};
+
 	const myImage = cld.image(uploadedImg);
 
 	return (
 		<>
 			<AdvancedImage
+				onClick={showImage}
 				className={className}
 				cldImg={myImage}
 				plugins={[lazyload(), placeholder({ mode: "blur" })]}
 			/>
+
+			{showModal && (
+				<Modal closeModal={closeModal} isImg={true}>
+					<AdvancedImage
+						className="max-w[700px] max-h-[700px] w-full h-full"
+						cldImg={myImage}
+						plugins={[lazyload(), placeholder({ mode: "blur" })]}
+					/>
+				</Modal>
+			)}
 		</>
 	);
 }
