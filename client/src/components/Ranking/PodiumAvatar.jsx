6cfd8380211b840f7f554c8f585213db4ca8d64a
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-16 20:08:34 +0200

    Usar imagen Cloudinary y ruta a user/id

--- client/src/components/Ranking/PodiumAvatar.jsx
+++ client/src/components/Ranking/PodiumAvatar.jsx
@@ -1,16 +1,22 @@
-export default function PodiumAvatar({ image, ranking }) {
+import Img from "../ui/CloudinaryImg";
+import { Link } from "react-router-dom";
+
+export default function PodiumAvatar({ userId, image, ranking }) {
 	return (
 		<div className="relative flex justify-center">
-			<img
-				className={`inline-block h-36 w-36 rounded-full filter brightness-50 contrast-150 ${
-					ranking === 1 ? "-mb-16" : "-mb-14"
-				}`}
-				src={image}
-				alt="avatar"
-			/>
-			<h1 className="absolute text-white font-bold pt-10 inset-0 text-3xl text-center ">
-				{ranking}
-			</h1>
+			<Link to={`/user/${userId}`}>
+				<Img
+					isContact={true}
+					className={`inline-block h-36 w-36 rounded-full filter brightness-50 contrast-150 ${
+						ranking === 1 ? "-mb-16" : "-mb-14"
+					}`}
+					uploadedImg={image}
+					alt="avatar"
+				/>
+				<h1 className="absolute text-white font-bold pt-10 inset-0 text-3xl text-center ">
+					{ranking}
+				</h1>
+			</Link>
 		</div>
 	);
 }
