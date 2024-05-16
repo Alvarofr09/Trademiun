28ef32242d6b4940c7a95da472da676904624df1
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-16 20:08:49 +0200

    pasar id del user

--- client/src/components/Ranking/TopOne.jsx
+++ client/src/components/Ranking/TopOne.jsx
@@ -1,10 +1,13 @@
 import PodiumAvatar from "./PodiumAvatar";
 
 export default function TopOne({ user, seguidores }) {
-	console.log(user);
 	return (
 		<div className="basis-1/3 flex flex-col justify-end">
-			<PodiumAvatar image={user.image} ranking={user.ranking} />
+			<PodiumAvatar
+				userId={user.id}
+				image={user.image}
+				ranking={user.ranking}
+			/>
 			<div className=" bg-primario h-52 pb-4 rounded-t-xl text-2xl flex justify-center text-center items-end text-white font-bold">
 				<div>
 					<p>{user.username}</p>
--- client/src/components/Ranking/TopThree.jsx
+++ client/src/components/Ranking/TopThree.jsx
@@ -3,7 +3,11 @@ import PodiumAvatar from "./PodiumAvatar";
 export default function TopThree({ user, seguidores }) {
 	return (
 		<div className="basis-1/3 flex flex-col justify-end">
-			<PodiumAvatar image={user.image} ranking={user.ranking} />
+			<PodiumAvatar
+				userId={user.id}
+				image={user.image}
+				ranking={user.ranking}
+			/>
 			<div className="bg-fondoWebApp h-36 pb-4 rounded-tr-xl text-2xl flex justify-center text-center items-end text-primario font-bold">
 				<div>
 					<p>{user.username}</p>
