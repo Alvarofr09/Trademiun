66dcac6520e8db6b1d1a8c9450e7a1868de93cea
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-05-17 20:27:36 +0200

    Ruta de unirse a grupos

--- client/src/api/APIRoutes.js
+++ client/src/api/APIRoutes.js
@@ -10,6 +10,7 @@ export const getUserInfo = `${host}/api/auth/getUserInfo`;
 export const getTopRentabilidad = `${host}/api/auth/get-users-by-rentabilidad`;
 export const getTopSeguidores = `${host}/api/auth/get-users-by-seguidores`;
 export const updateUserRoute = `${host}/api/auth/update-user`;
+export const hasGroupRoute = `${host}/api/auth/has-group`;
 
 export const sendMessageRoute = `${host}/api/messages/add-message`;
 export const getAllGroupMessages = `${host}/api/messages/getGroupMessages`;
