c4c561513d0233650930d6a7b5b8cfd4c241168d
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-05-17 13:55:24 +0200

    Comentarios

--- server/routers/userRoutes.js
+++ server/routers/userRoutes.js
@@ -11,14 +11,17 @@ const {
 
 const userRouter = express.Router();
 
+/*----------- Endpoints de POST -----------*/
 userRouter.post("/register", userRegister);
 userRouter.post("/login", userLogin);
 
+/*----------- Endpoints de GET -----------*/
 userRouter.get("/allUsers/:id", getAllUsers);
 userRouter.get("/getUserInfo/:id", getUser);
 userRouter.get("/get-users-by-rentabilidad", getUsersByRentabilidad);
 userRouter.get("/get-users-by-seguidores", getUsersBySeguidores);
 
+/*----------- Endpoints de PATCH -----------*/
 userRouter.patch("/update-user/:id", updateUser);
 
 module.exports = userRouter;
