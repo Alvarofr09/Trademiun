1792fc618622a6504226edeca2c2b4c2646338f2
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-19 11:00:42 +0200

    Quitar logout

--- client/src/components/Chat/Contacts.jsx
+++ client/src/components/Chat/Contacts.jsx
@@ -22,7 +22,7 @@ export default function Contacts({ contacts, changeChat }) {
 				<div className="basis-1/12 w-full">
 					<InputSearch />
 				</div>
-				<div className="contacts basis-10/12">
+				<div className="contacts basis-11/12">
 					{contacts.map((contact, index) => {
 						return (
 							<div
@@ -57,10 +57,6 @@ export default function Contacts({ contacts, changeChat }) {
 							AQUI
 						</Link>
 					</p>
-				</div>
-
-				<div className="usuario basis-1/12">
-					<Logout />
 				</div>
 			</div>
 		</>
