107eaff39ad4fdf5d8f990b3c2c8949ca63eeb4e
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-19 12:26:34 +0200

    Empezar a hacer con movil

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,24 +1,36 @@
 import { Outlet } from "react-router-dom";
 import { useAuthContext } from "../context/AuthContext";
 import SideBar from "./SideBar";
+import useDeviceType from "../hooks/useDeviceType";
+import BottomBar from "./BottomBar";
 
 export default function Layout() {
 	const { auth } = useAuthContext();
+	const isMobile = useDeviceType();
 	return (
 		<div>
 			<nav>
 				{auth ? (
-					<>
+					isMobile ? (
+						<div className="flex flex-col">
+							<main className="basis-10/12 mx-auto">
+								<Outlet />
+							</main>
+							<div className="basis-2/12 mx-auto">
+								<BottomBar />
+							</div>
+						</div>
+					) : (
+						// Contenido para usuarios autenticados en dispositivos de escritorio
 						<div className="flex">
 							<div className="basis-2/12 mx-auto">
 								<SideBar />
 							</div>
-
 							<main className="basis-10/12 mx-auto">
 								<Outlet />
 							</main>
 						</div>
-					</>
+					)
 				) : (
 					<main className="app">
 						<Outlet />
