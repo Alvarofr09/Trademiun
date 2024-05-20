4e0ad0584d82ba6c416ffa90a373a2f90a1266d2
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-20 23:52:14 +0200

    Quitar cosas de password

--- client/src/components/Formik/LoginSchema.js
+++ client/src/components/Formik/LoginSchema.js
@@ -4,12 +4,6 @@ const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
 //min 5 characters, 1 uppercase, 1 lowercase, 1 numeric digit
 
 export const LoginSchema = yup.object().shape({
-  email: yup.string().email("Email incorrecto").required("Email Requerido"),
-  password: yup
-    .string()
-    .matches(passwordRules, {
-      message:
-        "La contraseña debe tener al menos 5 caracteres, 1 mayúscula, 1 minúscula y 1 número",
-    })
-    .required("Password requerido"),
+	email: yup.string().email("Email incorrecto").required("Email Requerido"),
+	password: yup.string().required("Password requerido"),
 });
