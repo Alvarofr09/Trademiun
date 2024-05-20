30deedf8926c982259300eb10af648522f567323
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-20 20:10:43 +0200

    Hacer condicion para ver si estan en el o no

--- client/src/components/BottomBar.jsx
+++ client/src/components/BottomBar.jsx
@@ -1,15 +1,20 @@
+import { Link, useLocation } from "react-router-dom";
 const BottomBar = () => {
+	const location = useLocation();
 	return (
 		<footer className="py-4 bg-black sticky bottom-0">
 			<div className="mx-auto max-w-md h-8 flex items-center">
-				<div className="basis-1/4 flex justify-center hover:cursor-pointer">
+				<Link
+					to={"/"}
+					className="basis-1/4 flex justify-center hover:cursor-pointer"
+				>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						width="24"
 						height="24"
 						viewBox="0 0 24 24"
 						fill="none"
-						stroke="white"
+						stroke={`${location.pathname === "/" ? "#39BFF0" : "white"}`}
 						strokeWidth="2"
 						strokeLinecap="round"
 						className="icon icon-tabler icons-tabler-outline icon-tabler-message hover:stroke-secundario"
@@ -19,15 +24,20 @@ const BottomBar = () => {
 						<path d="M8 13h6" />
 						<path d="M18 4a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-5l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12z" />
 					</svg>
-				</div>
-				<div className="basis-1/4 flex justify-center hover:cursor-pointer">
+				</Link>
+				<Link
+					to={"/busqueda"}
+					className="basis-1/4 flex justify-center hover:cursor-pointer"
+				>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						width="24"
 						height="24"
 						viewBox="0 0 24 24"
 						fill="none"
-						stroke="white"
+						stroke={`${
+							location.pathname === "/busqueda" ? "#39BFF0" : "white"
+						}`}
 						strokeWidth="2"
 						strokeLinecap="round"
 						className="icon icon-tabler icons-tabler-outline icon-tabler-search hover:stroke-secundario"
@@ -36,15 +46,20 @@ const BottomBar = () => {
 						<path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
 						<path d="M21 21l-6 -6" />
 					</svg>
-				</div>
-				<div className="basis-1/4 flex justify-center hover:cursor-pointer">
+				</Link>
+				<Link
+					to={"/noticias"}
+					className="basis-1/4 flex justify-center hover:cursor-pointer"
+				>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						width="24"
 						height="24"
 						viewBox="0 0 24 24"
 						fill="none"
-						stroke="white"
+						stroke={`${
+							location.pathname === "/noticias" ? "#39BFF0" : "white"
+						}`}
 						strokeWidth="2"
 						strokeLinecap="round"
 						className="icon icon-tabler icons-tabler-outline icon-tabler-align-box-bottom-center hover:stroke-secundario"
@@ -55,15 +70,18 @@ const BottomBar = () => {
 						<path d="M12 11v6" />
 						<path d="M15 13v4" />
 					</svg>
-				</div>
-				<div className="basis-1/4 flex justify-center hover:cursor-pointer">
+				</Link>
+				<Link
+					to={"/cursos"}
+					className="basis-1/4 flex justify-center hover:cursor-pointer"
+				>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						width="24"
 						height="24"
 						viewBox="0 0 24 24"
 						fill="none"
-						stroke="white"
+						stroke={`${location.pathname === "/cursos" ? "#39BFF0" : "white"}`}
 						strokeWidth="2"
 						strokeLinecap="round"
 						className="icon icon-tabler icons-tabler-outline icon-tabler-message hover:stroke-secundario"
@@ -73,7 +91,7 @@ const BottomBar = () => {
 						<path d="M8 13h6" />
 						<path d="M18 4a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-5l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12z" />
 					</svg>
-				</div>
+				</Link>
 			</div>
 		</footer>
 	);
