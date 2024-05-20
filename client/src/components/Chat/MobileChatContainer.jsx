61518119b3a97270c0782eb61b43c58c56054b4d
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-20 23:51:52 +0200

    Crear el contenedor del chat pero en movil

---
+++ client/src/components/Chat/MobileChatContainer.jsx
@@ -0,0 +1,215 @@
+// MobileChatContainer.jsx
+import { useState, useEffect, useRef } from "react";
+import {
+	getAllGroupMessages,
+	getSignalsGroup,
+	isAdmin,
+	sendMessageRoute,
+} from "../../api/APIRoutes";
+import { userApi } from "../../api/APIRoutes";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import { useUserContext } from "../../context/UserContext";
+import ChatHeader from "./ChatHeader";
+import Messages from "./Messages";
+import ChatInput from "./ChatInput";
+import { format } from "date-fns";
+
+export default function MobileChatContainer({ currentChat, socket, onBack }) {
+	const { user } = useUserContext();
+	const [messages, setMessages] = useState([]);
+	const [isAdministrador, setIsAdministrador] = useState(false);
+	const [arrivalMessage, setArrivalMessage] = useState(null);
+	const scrollRef = useRef();
+
+	console.log(user);
+	useEffect(() => {
+		async function fetchData() {
+			if (!currentChat) return;
+			const token = localStorage.getItem("token");
+			// console.log(token);
+
+			const user = jwtDecode(token);
+
+			const { data } = await axios.post(`${isAdmin}/${user.id}`, {
+				group_id: currentChat.id,
+			});
+
+			if (data.isAdmin) {
+				setIsAdministrador(true);
+			} else {
+				setIsAdministrador(false);
+			}
+		}
+
+		fetchData();
+	}, [currentChat]);
+
+	useEffect(() => {
+		async function fetchData() {
+			if (!currentChat) return;
+			const mensajes = await userApi.post(getAllGroupMessages, {
+				from: user.id,
+				to: currentChat.id,
+			});
+
+			const signals = await userApi.post(getSignalsGroup, {
+				from: user.id,
+				to: currentChat.id,
+			});
+
+			console.log(signals.data);
+
+			const allMessages = mensajes.data.concat(signals.data);
+
+			const datos = allMessages.sort(
+				(a, b) => new Date(a.date) - new Date(b.date)
+			);
+			setMessages(datos);
+		}
+
+		fetchData();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [currentChat]);
+
+	useEffect(() => {
+		console.log(socket);
+		if (socket.current) {
+			socket.current.on("msg-recieve", (msg) => {
+				if (msg.type === "message") {
+					setArrivalMessage({
+						fromSelf: false,
+						username: user.username,
+						message: msg.message,
+						date: msg.date,
+						type: msg.type,
+					});
+				} else {
+					setArrivalMessage({
+						fromSelf: false,
+						username: user.username,
+						image: msg.image,
+						description: msg.description,
+						moneda: msg.moneda,
+						entrada: msg.entrada,
+						stopLoss: msg.stopLoss,
+						takeProfit: msg.takeProfit,
+						riesgo: msg.riesgo,
+						isCompra: msg.isCompra,
+						date: msg.date,
+						type: msg.type,
+					});
+				}
+			});
+		}
+	}, []);
+
+	useEffect(() => {
+		arrivalMessage && setMessages((prev) => [...prev, arrivalMessage]);
+	}, [arrivalMessage]);
+
+	useEffect(() => {
+		scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+	}, [messages]);
+
+	const handleSendMsg = async (msg) => {
+		const now = new Date();
+		const formattedDate = format(now, "yyyy-MM-dd HH:mm:ss");
+		try {
+			await axios.post(sendMessageRoute, {
+				from: user.id,
+				to: currentChat.id,
+				text: msg,
+			});
+
+			socket.current.emit("send-msg", {
+				to: currentChat.id,
+				from: user.id,
+				date: formattedDate,
+				username: user.username,
+				message: msg,
+				type: "message",
+			});
+
+			const msgs = [...messages];
+			msgs.push({
+				fromSelf: true,
+				message: msg,
+				date: formattedDate,
+				type: "message",
+				username: user.username,
+			});
+			setMessages(msgs);
+		} catch (error) {
+			alert(error);
+		}
+	};
+
+	const handleSendSignal = async (signal) => {
+		console.log(signal);
+		const {
+			sender_id,
+			group_id,
+			image,
+			description,
+			moneda,
+			entrada,
+			stopLoss,
+			takeProfit,
+			riesgo,
+			isCompra,
+		} = signal;
+
+		socket.current.emit("send-msg", {
+			to: group_id,
+			from: sender_id,
+			username: user.username,
+			date: Date.now(),
+			image,
+			description,
+			moneda,
+			entrada,
+			stopLoss,
+			takeProfit,
+			riesgo,
+			isCompra,
+			type: "signal",
+		});
+		const msgs = [...messages];
+		msgs.push({
+			fromSelf: true,
+			username: user.username,
+			date: Date.now(),
+			image,
+			description,
+			moneda,
+			entrada,
+			stopLoss,
+			takeProfit,
+			riesgo,
+			isCompra,
+			type: "signal",
+		});
+		setMessages(msgs);
+	};
+
+	return (
+		<div className="h-full centered bg-white">
+			{currentChat && (
+				<div
+					className="container h-full grid grid-rows-3 overflow-hidden"
+					style={{ gridTemplateRows: "11% 75% auto" }}
+				>
+					<ChatHeader onBack={onBack} currentChat={currentChat} />
+					<Messages messages={messages} scrollRef={scrollRef} />
+					<ChatInput
+						handleSendMsg={handleSendMsg}
+						isAdmin={isAdministrador}
+						currentChat={currentChat}
+						handleSendSignal={handleSendSignal}
+					/>
+				</div>
+			)}
+		</div>
+	);
+}
