67c51caa0205414f4af7b7d067782abfa97c5404
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-20 20:11:09 +0200

    Quitar el footer

--- client/src/views/Buscador.jsx
+++ client/src/views/Buscador.jsx
@@ -1,29 +1,28 @@
 import ButtonVerMas from "../components/ButtonVerMas";
 import CardMasRentables from "../components/CardMasRentables";
 import CardSeguidores from "../components/CardSeguidores";
-import Footer from "../components/Footer";
+
 import InputSearch from "../components/InputSearch";
 // import VerMasBoton from "../components/VerMasBoton";
 
 const Buscador = () => {
-  return (
-    <div className="mx-auto max-w-md h-3/5 bg-primario">
-      <InputSearch />
-      <h2 className="text-center text-2xl font-bold text-white p-4">
-        Top Seguidores
-      </h2>
-      <CardSeguidores />
-      <CardSeguidores />
-      <ButtonVerMas />
-      <h2 className="text-center text-2xl font-bold text-white p-4">
-        Más Rentables
-      </h2>
-      <CardMasRentables />
-      <CardMasRentables />
-      <ButtonVerMas />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="mx-auto max-w-md h-3/5 bg-primario">
+			<InputSearch />
+			<h2 className="text-center text-2xl font-bold text-white p-4">
+				Top Seguidores
+			</h2>
+			<CardSeguidores />
+			<CardSeguidores />
+			<ButtonVerMas />
+			<h2 className="text-center text-2xl font-bold text-white p-4">
+				Más Rentables
+			</h2>
+			<CardMasRentables />
+			<CardMasRentables />
+			<ButtonVerMas />
+		</div>
+	);
 };
 
 export default Buscador;
