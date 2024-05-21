1788215e7f952dabd70e711103b479b2038e9983
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-21 20:54:31 +0200

    Cambiar pixeles

--- client/src/hooks/useDeviceType.jsx
+++ client/src/hooks/useDeviceType.jsx
@@ -2,12 +2,12 @@ import { useState, useEffect } from "react";
 
 function useDeviceType() {
 	// Estado para almacenar si el dispositivo es móvil o no
-	const [isMobile, setIsMobile] = useState(window.innerWidth < 1024);
+	const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
 	useEffect(() => {
 		// Función para manejar el cambio de tamaño de la ventana
 		const handleResize = () => {
-			setIsMobile(window.innerWidth < 1024);
+			setIsMobile(window.innerWidth < 768);
 		};
 
 		// Agregar un event listener para el cambio de tamaño de la ventana
