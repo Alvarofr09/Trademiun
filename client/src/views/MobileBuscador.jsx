e064df84ff77b84ad1fb15f2c6dd4e5a62d63388
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-22 19:23:40 +0200

    Darle padding

--- client/src/views/MobileBuscador.jsx
+++ client/src/views/MobileBuscador.jsx
@@ -6,7 +6,7 @@ import InputSearch from "../components/ui/InputSearch";
 
 export default function MobileBuscador() {
 	return (
-		<div className="mx-auto max-w-md h-full bg-primario">
+		<div className="mx-auto max-w-md h-full bg-primario pb-20">
 			<InputSearch />
 			<h2 className="text-center text-2xl font-bold text-white p-4">
 				Top Seguidores
