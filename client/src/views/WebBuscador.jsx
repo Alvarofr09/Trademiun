083286480314bacc7678854240e3c195cf3bcd3d
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-05-22 13:58:20 +0200

    Hacer que busque los grupos por nombre

--- client/src/views/WebBuscador.jsx
+++ client/src/views/WebBuscador.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
-import InputSearch from "../components/InputSearch";
+import InputSearch from "../components/ui/InputSearch";
 // import WebNotificaciones from "../components/WebNotificaciones";
 import {
 	getAllGroups,
+	getGroupsByName,
 	getTopRentabilidad,
 	getTopSeguidores,
 	userApi,
@@ -10,6 +11,7 @@ import {
 import RestRankingTable from "../components/Ranking/RestRankingTable";
 import Podium from "../components/Ranking/Podium";
 import Img from "../components/ui/CloudinaryImg";
+import { Link } from "react-router-dom";
 
 const WebBuscador = () => {
 	const [mostrarSeguidores, setMostrarSeguidores] = useState(true);
@@ -17,6 +19,29 @@ const WebBuscador = () => {
 	const [topThree, setTopThree] = useState([]);
 	const [restoRanking, setRestoRanking] = useState([]);
 	const [grupos, setGrupos] = useState([]);
+	const [visibleGroups, setVisibleGroups] = useState(3);
+
+	const handleSearch = async (groupName) => {
+		try {
+			let data;
+
+			if (groupName === "") {
+				const response = await userApi.get(getAllGroups);
+				data = response.data;
+			} else {
+				const response = await userApi.get(`${getGroupsByName}/${groupName}`);
+				data = response.data;
+			}
+
+			setGrupos(data.groups);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleShowMore = () => {
+		setVisibleGroups((prevVisibleGroups) => prevVisibleGroups + 5);
+	};
 
 	useEffect(() => {
 		async function fetchData() {
@@ -70,8 +95,8 @@ const WebBuscador = () => {
 	};
 
 	return (
-		<div className="flex h-full">
-			<div className="basis-8/12 mx-auto border-x-2 border-x-primario overflow-y-scroll custom-scrollbar">
+		<div className="flex h-full ">
+			<div className="basis-8/12 mx-auto border-x-2 border-x-primario">
 				<div className="flex justify-center  py-12 gap-36 2xl:gap-48">
 					<button
 						className={`text-primario text-3xl px-4 py-2 font-bold ${
@@ -102,8 +127,8 @@ const WebBuscador = () => {
 				</div>
 			</div>
 
-			<div className="basis-4/12 mx-auto bg-white overflow-y-scroll custom-scrollbar">
-				<InputSearch />
+			<div className="basis-4/12 mx-auto bg-white relative">
+				<InputSearch handleSearch={handleSearch} />
 				{/* <InputSearch />
 				<WebNotificaciones
 					nombre="JuanJo Trader"
@@ -115,27 +140,40 @@ const WebBuscador = () => {
 					notificacion="HA GANADO HOY 3 TRADES"
 					foto="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
 				/> */}
-				{grupos.map((grupo, index) => (
-					<div
-						className={`contact mx-auto`}
-						key={index}
-						onClick={() => alert("hola")}
-					>
-						{grupo.image && (
-							<div className="avatar">
-								<Img
-									isContact={true}
-									className="h-14 w-14 avatar-image"
-									uploadedImg={grupo.image}
-									alt="avatar"
-								/>
+				<div className="contacts ">
+					{grupos.slice(0, visibleGroups).map((grupo, index) => (
+						<Link
+							className={`contact mx-auto`}
+							key={index}
+							to={`/user/${grupo.admin_id}`}
+						>
+							{grupo.image && (
+								<div className="avatar">
+									<Img
+										isContact={true}
+										className="h-14 w-14 avatar-image"
+										uploadedImg={grupo.image}
+										alt="avatar"
+									/>
+								</div>
+							)}
+							<div className="username ">
+								<h3 className="text-xl bold">{grupo.group_name}</h3>
+								<h3 className=" bold">Participantes: {grupo.participantes}</h3>
 							</div>
-						)}
-						<div className="  username">
-							<h3 className=" text-xl  bold">{grupo.group_name}</h3>
+						</Link>
+					))}
+					{visibleGroups < grupos.length && (
+						<div className="flex justify-center my-4">
+							<button
+								className="text-secundario text-xl px-4 py-2 font-bold "
+								onClick={handleShowMore}
+							>
+								Ver más
+							</button>
 						</div>
-					</div>
-				))}
+					)}
+				</div>
 			</div>
 		</div>
 	);
