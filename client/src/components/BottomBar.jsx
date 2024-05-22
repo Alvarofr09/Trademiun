ce47d22f7ace476477e15175460c083adf8cf052
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-22 19:06:22 +0200

    Hacer los navs con un map

--- client/src/components/BottomBar.jsx
+++ client/src/components/BottomBar.jsx
@@ -1,80 +1,45 @@
 import { Link, useLocation } from "react-router-dom";
-import { IconMessage, IconMessageFilled } from "@tabler/icons-react";
-import { IconSearch, IconZoomFilled } from "@tabler/icons-react";
-import { IconNews, IconArticleFilled } from "@tabler/icons-react";
-import { IconPlayerPlay, IconPlayerPlayFilled } from "@tabler/icons-react";
-import { IconUser, IconUserFilled } from "@tabler/icons-react";
-
+import {
+	IconMessage,
+	IconMessageFilled,
+	IconSearch,
+	IconZoomFilled,
+	IconNews,
+	IconArticleFilled,
+	IconPlayerPlay,
+	IconPlayerPlayFilled,
+	IconUser,
+	IconUserFilled,
+} from "@tabler/icons-react";
 import { useUserContext } from "../context/UserContext";
 
 export default function BottomBar() {
 	const { user } = useUserContext();
 	const location = useLocation();
+	const routes = [
+		{ path: "/", icon: IconMessage, iconFilled: IconMessageFilled },
+		{ path: "/busqueda", icon: IconSearch, iconFilled: IconZoomFilled },
+		{ path: "/noticias", icon: IconNews, iconFilled: IconArticleFilled },
+		{ path: "/cursos", icon: IconPlayerPlay, iconFilled: IconPlayerPlayFilled },
+		{ path: `/user/${user.id}`, icon: IconUser, iconFilled: IconUserFilled },
+	];
+
 	return (
 		<footer className="py-4 bg-black sticky bottom-0">
-			<div className="mx-auto max-w-md h-8 flex items-center">
-				<Link
-					to={"/"}
-					className="basis-1/5 flex justify-center hover:cursor-pointer"
-				>
-					<div>
-						{location.pathname === "/" ? (
-							<IconMessageFilled size={24} color="#39BFF0" />
+			<div className="mx-auto max-w-md h-8 flex items-center justify-between">
+				{routes.map(({ path, icon: Icon, iconFilled: IconFilled }) => (
+					<Link
+						key={path}
+						to={path}
+						className="flex-grow flex justify-center hover:cursor-pointer"
+					>
+						{location.pathname === path ? (
+							<IconFilled size={24} color="#39BFF0" />
 						) : (
-							<IconMessage size={24} color="#ffffff" />
+							<Icon size={24} color="#ffffff" />
 						)}
-					</div>
-				</Link>
-				<Link
-					to={"/busqueda"}
-					className="basis-1/5 flex justify-center hover:cursor-pointer"
-				>
-					<div>
-						{location.pathname === "/busqueda" ? (
-							<IconZoomFilled size={24} color="#39BFF0" />
-						) : (
-							<IconSearch size={24} color="#ffffff" />
-						)}
-					</div>
-				</Link>
-				<Link
-					to={"/noticias"}
-					className="basis-1/5 flex justify-center hover:cursor-pointer"
-				>
-					<div>
-						{location.pathname === "/noticias" ? (
-							<IconArticleFilled size={24} color="#39BFF0" />
-						) : (
-							<IconNews size={24} color="#ffffff" />
-						)}
-					</div>
-				</Link>
-
-				<Link
-					to={"/cursos"}
-					className="basis-1/5 flex justify-center hover:cursor-pointer"
-				>
-					<div>
-						{location.pathname === "/cursos" ? (
-							<IconPlayerPlayFilled size={24} color="#39BFF0" />
-						) : (
-							<IconPlayerPlay size={24} color="#ffffff" />
-						)}
-					</div>
-				</Link>
-
-				<Link
-					to={`/user/${user.id}`}
-					className="basis-1/5 flex justify-center hover:cursor-pointer"
-				>
-					<div>
-						{location.pathname === `/user/${user.id}` ? (
-							<IconUserFilled size={24} color="#39BFF0" />
-						) : (
-							<IconUser size={24} color="#ffffff" />
-						)}
-					</div>
-				</Link>
+					</Link>
+				))}
 			</div>
 		</footer>
 	);
--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,197 +1,117 @@
 import { Link, useLocation } from "react-router-dom";
 import logo_negro from "../assets/img/logo_negro.png";
 import LogoChico from "../assets/img/LogoChico.png";
-
-import { IconMessage, IconMessageFilled } from "@tabler/icons-react";
-import { IconSearch, IconZoomFilled } from "@tabler/icons-react";
-import { IconNews, IconArticleFilled } from "@tabler/icons-react";
 import {
+	IconMessage,
+	IconMessageFilled,
+	IconSearch,
+	IconZoomFilled,
+	IconNews,
+	IconArticleFilled,
 	IconHexagonLetterB,
 	IconHexagonLetterBFilled,
+	IconUser,
+	IconUserFilled,
+	IconPlayerPlay,
+	IconPlayerPlayFilled,
+	IconCreditCard,
+	IconCreditCardFilled,
+	IconLogout,
 } from "@tabler/icons-react";
-import { IconUser, IconUserFilled } from "@tabler/icons-react";
-import { IconPlayerPlay, IconPlayerPlayFilled } from "@tabler/icons-react";
-import { IconCreditCard, IconCreditCardFilled } from "@tabler/icons-react";
-import { IconLogout } from "@tabler/icons-react";
-
 import { useUserContext } from "../context/UserContext";
 import { useAuthContext } from "../context/AuthContext";
 
-const SideBar = () => {
+export default function SideBar() {
 	const { user } = useUserContext();
 	const { logout } = useAuthContext();
 	const location = useLocation();
+
+	const routes = [
+		{
+			path: "/",
+			label: "Chats",
+			icon: IconMessage,
+			iconFilled: IconMessageFilled,
+		},
+		{
+			path: "/busqueda",
+			label: "Búsqueda",
+			icon: IconSearch,
+			iconFilled: IconZoomFilled,
+		},
+		{
+			path: "/noticias",
+			label: "Noticias",
+			icon: IconNews,
+			iconFilled: IconArticleFilled,
+		},
+		{
+			path: "/blog",
+			label: "Blog",
+			icon: IconHexagonLetterB,
+			iconFilled: IconHexagonLetterBFilled,
+		},
+		{
+			path: `/user/${user.id}`,
+			label: "User",
+			icon: IconUser,
+			iconFilled: IconUserFilled,
+		},
+		{
+			path: "/cursos",
+			label: "Cursos",
+			icon: IconPlayerPlay,
+			iconFilled: IconPlayerPlayFilled,
+		},
+		{
+			path: "/pagos",
+			label: "Pagos",
+			icon: IconCreditCard,
+			iconFilled: IconCreditCardFilled,
+		},
+	];
+
 	return (
-		<>
-			<div className="max-w-68 h-lvh sticky md:text-base lg:text-lg text-2xl top-0 bg-white">
-				{/* Sidebar */}
-				<div className="hidden lg:block py-8 w-36 xl:w-44 ml-12 mr-2">
-					<img className="" src={logo_negro} alt="logo" />
+		<div className="max-w-68 h-lvh sticky md:text-base lg:text-lg text-2xl top-0 bg-white">
+			{/* Sidebar */}
+			<div className="hidden lg:block py-8 w-36 xl:w-44 ml-12 mr-2">
+				<img src={logo_negro} alt="logo" />
+			</div>
+			<img
+				className="ml-4 md:ml-6 lg:ml-12 py-8 block lg:hidden"
+				src={LogoChico}
+				alt="Logo"
+			/>
+
+			{routes.map(({ path, label, icon: Icon, iconFilled: IconFilled }) => (
+				<Link key={path} to={path} className="flex hover:cursor-pointer mb-8">
+					<div>
+						{location.pathname === path ? (
+							<IconFilled size={24} className="ml-4 md:ml-6 lg:ml-12" />
+						) : (
+							<Icon size={24} className="ml-4 md:ml-6 lg:ml-12" />
+						)}
+					</div>
+					<div>
+						<p
+							className={`md:hidden lg:block ml-4 ${
+								location.pathname === path && "font-bold"
+							}`}
+						>
+							{label}
+						</p>
+					</div>
+				</Link>
+			))}
+
+			<div onClick={logout} className="flex hover:cursor-pointer mb-8">
+				<div>
+					<IconLogout size={24} className="ml-4 md:ml-6 lg:ml-12" />
 				</div>
-				<img
-					className="ml-4 md:ml-6 lg:ml-12 py-8 block lg:hidden"
-					src={LogoChico}
-					alt="Logo"
-				/>
-
-				<Link to={"/"} className="flex hover:cursor-pointer mb-8">
-					<div>
-						{location.pathname === "/" ? (
-							<IconMessageFilled size={24} className="ml-4 md:ml-6 lg:ml-12" />
-						) : (
-							<IconMessage size={24} className="ml-4 md:ml-6 lg:ml-12" />
-						)}
-					</div>
-					<div>
-						<p
-							className={`md:hidden lg:block ml-4 ${
-								location.pathname === "/" && "font-bold"
-							}`}
-						>
-							Chats
-						</p>
-					</div>
-				</Link>
-
-				<Link to={"/busqueda"} className="flex hover:cursor-pointer mb-8">
-					<div>
-						{location.pathname === "/busqueda" ? (
-							<IconZoomFilled size={24} className="ml-4 md:ml-6 lg:ml-12" />
-						) : (
-							<IconSearch size={24} className="ml-4 md:ml-6 lg:ml-12" />
-						)}
-					</div>
-					<div>
-						<p
-							className={`md:hidden lg:block ml-4 ${
-								location.pathname === "/busqueda" && "font-bold"
-							}`}
-						>
-							Búsqueda
-						</p>
-					</div>
-				</Link>
-
-				<Link to={"/noticias"} className="flex hover:cursor-pointer mb-8">
-					<div>
-						{location.pathname === "/noticias" ? (
-							<IconArticleFilled size={24} className="ml-4 md:ml-6 lg:ml-12" />
-						) : (
-							<IconNews size={24} className="ml-4 md:ml-6 lg:ml-12" />
-						)}
-					</div>
-					<div>
-						<p
-							className={`md:hidden lg:block ml-4 ${
-								location.pathname === "/noticias" && "font-bold"
-							}`}
-						>
-							Noticias
-						</p>
-					</div>
-				</Link>
-
-				<Link to={"/blog"} className="flex hover:cursor-pointer mb-8">
-					<div>
-						{location.pathname === "/blog" ? (
-							<IconHexagonLetterBFilled
-								size={24}
-								className="ml-4 md:ml-6 lg:ml-12"
-							/>
-						) : (
-							<IconHexagonLetterB size={24} className="ml-4 md:ml-6 lg:ml-12" />
-						)}
-					</div>
-					<div>
-						<p
-							className={`md:hidden lg:block ml-4 ${
-								location.pathname === "/blog" && "font-bold"
-							}`}
-						>
-							Blog
-						</p>
-					</div>
-				</Link>
-
-				<Link
-					to={`/user/${user.id}`}
-					className="flex hover:cursor-pointer mb-8"
-				>
-					<div>
-						{location.pathname === `/user/${user.id}` ? (
-							<IconUserFilled size={24} className="ml-4 md:ml-6 lg:ml-12" />
-						) : (
-							<IconUser size={24} className="ml-4 md:ml-6 lg:ml-12" />
-						)}
-					</div>
-					<div>
-						<p
-							className={`md:hidden lg:block ml-4 ${
-								location.pathname === `/user/${user.id}` && "font-bold"
-							}`}
-						>
-							User
-						</p>
-					</div>
-				</Link>
-
-				<Link to={"/cursos"} className="flex hover:cursor-pointer mb-8">
-					<div>
-						{location.pathname === "/cursos" ? (
-							<IconPlayerPlayFilled
-								size={24}
-								className="ml-4 md:ml-6 lg:ml-12"
-							/>
-						) : (
-							<IconPlayerPlay size={24} className="ml-4 md:ml-6 lg:ml-12" />
-						)}
-					</div>
-					<div>
-						<p
-							className={`md:hidden lg:block ml-4 ${
-								location.pathname === "/cursos" && "font-bold"
-							}`}
-						>
-							Cursos
-						</p>
-					</div>
-				</Link>
-
-				<Link to={"/pagos"} className="flex hover:cursor-pointer mb-8">
-					<div>
-						{location.pathname === "/pagos" ? (
-							<IconCreditCardFilled
-								size={24}
-								className="ml-4 md:ml-6 lg:ml-12"
-							/>
-						) : (
-							<IconCreditCard size={24} className="ml-4 md:ml-6 lg:ml-12" />
-						)}
-					</div>
-					<div>
-						<p
-							className={`md:hidden lg:block ml-4 ${
-								location.pathname === "/pagos" && "font-bold"
-							}`}
-						>
-							Pagos
-						</p>
-					</div>
-				</Link>
-				<div
-					onClick={() => logout()}
-					className="flex hover:cursor-pointer mb-8"
-				>
-					<div>
-						<IconLogout size={24} className="ml-4 md:ml-6 lg:ml-12" />
-					</div>
-					<div>
-						<p className={`md:hidden lg:block ml-4 `}>Logout </p>
-					</div>
+				<div>
+					<p className="md:hidden lg:block ml-4">Logout</p>
 				</div>
 			</div>
-		</>
+		</div>
 	);
-};
-
-export default SideBar;
+}
