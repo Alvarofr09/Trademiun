09a3e17db1e2265572aab82f2850e6b1ce32a98e
PabloFC <pablofc_68@hotmail.com>
2024-05-23 11:11:38 +0200

    cambios realizados en App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,22 @@
 import "./App.css";
-import MovilPerfil from "./views/MovilPerfil";
-// import { Routes, Route } from "react-router-dom";
-// import Layout from "./components/Layout";
-// import Login from "./views/Login";
-// import RequireAuth from "./components/RequireAuth";
-// import WebChat from "./views/WebChat";
-// import UserDetails from "./views/UserDetails";
+// import MovilPerfil from "./views/MovilPerfil";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Login from "./views/Login";
+import RequireAuth from "./components/RequireAuth";
+import WebChat from "./views/WebChat";
+import UserDetails from "./views/UserDetails";
 
-// import Busqueda from "./views/Busqueda";
-// import CommingSoon from "./views/CommingSoon";
-// import WebBlog from "./views/WebBlog";
-// import WebPagos from "./views/WebPagos";
-// import WebNoticias from "./views/WebNoticias";
+import Busqueda from "./views/Busqueda";
+import CommingSoon from "./views/CommingSoon";
+import WebBlog from "./views/WebBlog";
+import WebPagos from "./views/WebPagos";
+import WebNoticias from "./views/WebNoticias";
 function App() {
   return (
     <>
-      <MovilPerfil />
-      {/* <Routes>
+      {/* <MovilPerfil /> */}
+      <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="login" element={<Login />} />
 
@@ -30,7 +30,7 @@ function App() {
             <Route path="/pagos" element={<WebPagos />} />
           </Route>
         </Route>
-      </Routes> */}
+      </Routes>
     </>
   );
 }
