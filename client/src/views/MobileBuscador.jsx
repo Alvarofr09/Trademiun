ba7a8fa14f56368c46912515f1d44ce3a7875919
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-05-24 20:08:40 +0200

    Hacerl el handleSearch

--- client/src/views/MobileBuscador.jsx
+++ client/src/views/MobileBuscador.jsx
@@ -4,13 +4,16 @@ import InputSearch from "../components/ui/InputSearch";
 import {
 	getTopRentabilidad,
 	getTopSeguidores,
+	getUsersByNameRoute,
 	userApi,
 } from "../api/APIRoutes";
 import { IconArrowUp, IconArrowDown } from "@tabler/icons-react";
 import { Link } from "react-router-dom";
 import CardUser from "../components/CardUser";
+import Img from "../components/ui/CloudinaryImg";
 
 export default function MobileBuscador() {
+	const [users, setUsers] = useState([]);
 	const [usersBySeguidores, setUsersBySeguidores] = useState([]);
 	const [usersByRentabilidad, setUsersByRentabilidad] = useState([]);
 	const [visibleUsersBySeguidores, setVisibleUsersBySeguidores] = useState(2);
@@ -32,6 +35,25 @@ export default function MobileBuscador() {
 		setVisibleUsersByRentabilidad((prevVisibleGroups) => prevVisibleGroups - 3);
 	};
 
+	const handleSearch = async (userName) => {
+		try {
+			let data;
+
+			if (userName === "") {
+				data = { users: [] };
+			} else {
+				const response = await userApi.get(
+					`${getUsersByNameRoute}/${userName}`
+				);
+				data = response.data;
+			}
+
+			setUsers(data.users);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	useEffect(() => {
 		async function fetchData() {
 			try {
@@ -51,7 +73,34 @@ export default function MobileBuscador() {
 
 	return (
 		<div className="mx-autow-full h-full bg-primario pb-20">
-			<InputSearch />
+			<InputSearch handleSearch={handleSearch} />
+			{users.length > 0 && (
+				<div className="centered flex-col">
+					{users.slice(0, 3).map((user, index) => {
+						return (
+							<Link
+								className={`contact !flex-row centered !border-white`}
+								key={index}
+								to={`/user/${user.id}`}
+							>
+								{user.image && (
+									<div className="avatar">
+										<Img
+											isContact={true}
+											className="h-14 w-14 avatar-image"
+											uploadedImg={user.image}
+											alt="avatar"
+										/>
+									</div>
+								)}
+								<div className="  username">
+									<h3 className=" text-xl text-white bold">{user.username}</h3>
+								</div>
+							</Link>
+						);
+					})}
+				</div>
+			)}
 			<h2 className="text-center text-2xl font-bold text-white p-4">
 				Top Seguidores
 			</h2>
@@ -101,7 +150,7 @@ export default function MobileBuscador() {
 				<div className="flex justify-center my-4">
 					<button
 						className="text-secundario flex flex-row text-xl px-4 py-2 font-bold "
-						onClick={handleShowMoreSeguidores}
+						onClick={handleShowMoreRentabilidad}
 					>
 						Ver más <IconArrowDown color="#39BFF0" />
 					</button>
