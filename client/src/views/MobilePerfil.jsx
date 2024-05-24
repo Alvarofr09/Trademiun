1f4f9a583b139b26a59eda4df978275ff8437ce6
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-05-24 14:09:48 +0200

    Arreglar mobilePerfil y hacer la logica

--- client/src/views/MobilePerfil.jsx
+++ client/src/views/MobilePerfil.jsx
@@ -2,7 +2,12 @@ import { useState, useRef, useEffect } from "react";
 import Img from "../components/ui/CloudinaryImg";
 import RangeInput from "../components/ui/RangeInput";
 import { useUserContext } from "../context/UserContext";
-import { IconEdit, IconCheck, IconFilePlus } from "@tabler/icons-react";
+import {
+	IconEdit,
+	IconCheck,
+	IconFilePlus,
+	IconLogout,
+} from "@tabler/icons-react";
 import { previewFiles } from "../utils/previewFile";
 import { useAuthContext } from "../context/AuthContext";
 import {
@@ -21,6 +26,9 @@ import {
 } from "../api/APIRoutes";
 import { useNavigate, useParams } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
+import Modal from "../components/Modal";
+import GroupForm from "../components/GroupForm/GroupForm";
+import Signal from "../components/ui/Signal";
 
 const toastConfig = {
 	position: "bottom-right",
@@ -31,9 +39,9 @@ const toastConfig = {
 };
 
 export default function MobilePerfil() {
-	const { login } = useAuthContext();
-	const { user } = useUserContext();
 	const { id } = useParams();
+	const { login, logout } = useAuthContext();
+	const { user } = useUserContext();
 	const navigate = useNavigate();
 	const [isCurrentUser, setIsCurrentUser] = useState(false);
 	const [userData, setUserData] = useState(null);
@@ -106,13 +114,11 @@ export default function MobilePerfil() {
 	}, [id, user, userGroup]);
 
 	const showGroupForm = () => setShowGroupModal(true);
-	const showUserForm = () => setShowUserModal(true);
 	const showJoinForm = () => setShowJoinModal(true);
 	const showLeaveForm = () => setShowLeaveModal(true);
 	const showDeleteForm = () => setShowDeleteModal(true);
 	const closeModal = () => {
 		setShowGroupModal(false);
-		setShowUserModal(false);
 		setShowJoinModal(false);
 		setShowLeaveModal(false);
 		setShowDeleteModal(false);
@@ -219,7 +225,7 @@ export default function MobilePerfil() {
 	}, [userData]);
 
 	async function handleSubmit() {
-		const { data } = await userApi.patch(`${updateUserRoute}/${user.id}`, {
+		const { data } = await userApi.patch(`${updateUserRoute}/${userData.id}`, {
 			username: userName,
 			image,
 			description,
@@ -241,149 +247,250 @@ export default function MobilePerfil() {
 		setEditing(true);
 	}
 
-	console.log("user data", userData);
-
 	return (
 		<>
 			<main className="relative mx-auto w-full h-full bg-primario pb-20">
-				<div className="absolute top-0 right-0 p-4">
-					{editing ? (
-						<IconCheck color="#ffffff" onClick={handleSubmit} />
-					) : (
-						<IconEdit color="#ffffff" onClick={handleStartEditing} />
-					)}
-				</div>
-				<div className="flex justify-center pt-8">
-					{!editing ? (
-						<Img
-							uploadedImg={userData.image}
-							alt="foto perfil"
-							className={"w-40 h-40 rounded-full"}
-						/>
-					) : image ? (
-						<div className="centered">
-							<img
-								src={image}
-								alt="Preview"
-								className="h-36 w-36 rounded-full"
-							/>
-						</div>
-					) : (
-						<div className="relative flex justify-center">
-							<Img
-								isContact={true}
-								className={`inline-block h-40 w-40 rounded-full filter brightness-50 contrast-150`}
-								uploadedImg={userData.image}
-								alt="avatar"
-							/>
-							<label
-								htmlFor="signalImage"
-								className="absolute inset-0 centered"
-							>
-								<IconFilePlus size={50} color="#fff" />
-							</label>
-							<input
-								type="file"
-								name="fileInput"
-								id="signalImage"
-								onChange={handleChange}
-								accept="image/png, image/jpeg, image/jpg, image/svg, image/ico, image/jfif, image/webp"
-								className="appearance-none hidden opacity-0"
-							/>
-						</div>
-					)}
-				</div>
-
-				<div className="centered flex-col py-4 gap-3 ">
-					<input
-						ref={inputRef}
-						type="text"
-						className={`text-center bg-primario w-fit  text-white text-3xl font-bold focus:border-white ${
-							editing ? "border-2 border-white " : "border-none"
-						}`}
-						value={userName}
-						onChange={(e) => setUserName(e.target.value)}
-						disabled={!editing}
-					/>
-				</div>
-				<div className="">
-					<h4 className="text-center text-tipografia text-sm mb-4">
-						{userData.seguidores} seguidores
-					</h4>
-				</div>
-
-				<div className="user-options centered flex-col gap-4">
-					{isCurrentUser ? (
-						<>
-							<button className="btn-dark" onClick={showUserForm}>
-								Editar Perfil
-							</button>
-							{!hasGroup ? (
-								<button className="btn-dark" onClick={showGroupForm}>
-									Crear Grupo
-								</button>
-							) : (
-								<button className="btn-dark" onClick={showDeleteForm}>
-									Borrar Grupo
-								</button>
-							)}
-						</>
-					) : (
-						<>
-							{!isFollowing ? (
-								<button className="btn-dark" onClick={handleFollow}>
-									Seguir
-								</button>
-							) : (
+				{userData && (
+					<>
+						<div className="absolute top-0 right-0 p-4">
+							{isCurrentUser && (
 								<>
-									<button className="btn-dark" onClick={handleUnfollow}>
-										Dejar de Seguir
-									</button>
-									{hasGroup &&
-										(!isInGroup ? (
-											<button className="btn-dark" onClick={showJoinForm}>
-												Unirse a grupo
-											</button>
-										) : (
-											<button className="btn-dark" onClick={showLeaveForm}>
-												Salir del grupo
-											</button>
-										))}
+									<IconLogout
+										color="#ffffff"
+										onClick={logout}
+										className="mb-4"
+									/>
+									{editing ? (
+										<IconCheck color="#ffffff" onClick={handleSubmit} />
+									) : (
+										<IconEdit color="#ffffff" onClick={handleStartEditing} />
+									)}
 								</>
 							)}
-						</>
-					)}
-				</div>
+						</div>
+						<div className="flex justify-center pt-8">
+							{!editing ? (
+								<Img
+									uploadedImg={user.image}
+									alt="foto perfil"
+									className={"w-40 h-40 rounded-full"}
+								/>
+							) : image ? (
+								<div className="centered">
+									<img
+										src={image}
+										alt="Preview"
+										className="h-36 w-36 rounded-full"
+									/>
+								</div>
+							) : (
+								<div className="relative flex justify-center">
+									<Img
+										isContact={true}
+										className={`inline-block h-40 w-40 rounded-full filter brightness-50 contrast-150`}
+										uploadedImg={userData.image}
+										alt="avatar"
+									/>
+									<label
+										htmlFor="signalImage"
+										className="absolute inset-0 centered"
+									>
+										<IconFilePlus size={50} color="#fff" />
+									</label>
+									<input
+										type="file"
+										name="fileInput"
+										id="signalImage"
+										onChange={handleChange}
+										accept="image/png, image/jpeg, image/jpg, image/svg, image/ico, image/jfif, image/webp"
+										className="appearance-none hidden opacity-0"
+									/>
+								</div>
+							)}
+						</div>
 
-				<div className="p-4">
-					<RangeInput title={"Maximo Drawdown"} value={66.6} max={100} />
-					<RangeInput title={"Riesgo/Beneficio medio"} value={3.5} max={4} />
-					<RangeInput title={"% Mensual medio"} value={66.6} max={100} />
-					<RangeInput title={"% Riesgo medio/trade"} value={311} max={311} />
-				</div>
+						<div className="centered flex-col py-4 gap-3 ">
+							<input
+								ref={inputRef}
+								type="text"
+								className={`text-center bg-primario w-fit text-white text-3xl font-bold focus:border-white ${
+									editing ? "border-2 border-white " : "border-none"
+								}`}
+								value={userName}
+								onChange={(e) => setUserName(e.target.value)}
+								disabled={!editing}
+							/>
+						</div>
+						<div className="">
+							<h4 className="text-center text-tipografia text-sm mb-4">
+								{userData.seguidores} seguidores
+							</h4>
+						</div>
 
-				{/* <ProgressBar titulo="Máximo Drawdown" porcentaje="66.6%" />
-			<ProgressBar titulo="Riesgo/Beneficio medio" porcentaje="90.7%" />
-			<ProgressBar titulo="% Mensual medio" porcentaje="77%" />
-			<ProgressBar titulo="% Riesgo medio/trade" porcentaje="83.3%" /> */}
+						<div className="user-options centered flex-col gap-4">
+							{isCurrentUser ? (
+								<>
+									{!hasGroup ? (
+										<button className="btn-dark" onClick={showGroupForm}>
+											Crear Grupo
+										</button>
+									) : (
+										<button className="btn-dark" onClick={showDeleteForm}>
+											Borrar Grupo
+										</button>
+									)}
+								</>
+							) : (
+								<>
+									{!isFollowing ? (
+										<button className="btn-dark" onClick={handleFollow}>
+											Seguir
+										</button>
+									) : (
+										<>
+											<button className="btn-dark" onClick={handleUnfollow}>
+												Dejar de Seguir
+											</button>
+											{hasGroup &&
+												(!isInGroup ? (
+													<button className="btn-dark" onClick={showJoinForm}>
+														Unirse a grupo
+													</button>
+												) : (
+													<button className="btn-dark" onClick={showLeaveForm}>
+														Salir del grupo
+													</button>
+												))}
+										</>
+									)}
+								</>
+							)}
+						</div>
 
-				<div className="mb-4">
-					<textarea
-						className={`w-full bg-primario text-white p-2 rounded-lg focus:border-white ${
-							editing ? "border-2 border-white " : "border-none"
-						}`}
-						rows="4"
-						value={description}
-						onChange={(e) => setDescripcion(e.target.value)}
-						disabled={!editing}
-					/>
-				</div>
+						<div className="p-4">
+							<RangeInput title={"Maximo Drawdown"} value={66.6} max={100} />
+							<RangeInput
+								title={"Riesgo/Beneficio medio"}
+								value={3.5}
+								max={4}
+							/>
+							<RangeInput title={"% Mensual medio"} value={66.6} max={100} />
+							<RangeInput
+								title={"% Riesgo medio/trade"}
+								value={311}
+								max={311}
+							/>
+						</div>
 
-				<div>
-					<button className="text-white bg-secundario px-4 py-2 rounded-xl ml-2">
-						Añadir trade
-					</button>
-				</div>
+						<div className="mb-4">
+							<textarea
+								className={`w-full bg-primario text-white p-2 rounded-lg focus:border-white ${
+									editing ? "border-2 border-white " : "border-none"
+								}`}
+								rows="4"
+								value={description}
+								onChange={(e) => setDescripcion(e.target.value)}
+								disabled={!editing}
+							/>
+						</div>
+
+						<div>
+							<button className="text-white bg-secundario px-4 py-2 rounded-xl ml-2">
+								Añadir trade
+							</button>
+							{signals.length === 0 ? (
+								<h3 className="mt-10 text-xl text-white centered">
+									No hay trades
+								</h3>
+							) : (
+								signals.map((signal) => {
+									return <Signal key={signal.id} signal={signal} />;
+								})
+							)}
+						</div>
+
+						{showGroupModal && (
+							<Modal closeModal={closeModal} isImg={false} title="Crear Grupo">
+								<GroupForm />
+							</Modal>
+						)}
+
+						{showJoinModal && (
+							<Modal
+								closeModal={closeModal}
+								isImg={false}
+								title={`Unirse a grupo de ${userData.username}?`}
+							>
+								<div className="gap-4 centered">
+									<button
+										onClick={() => handleJoinGroup()}
+										className="text-white px-8 py-4 border-none font-bold cursor-pointer rounded-[30px] text-xl uppercase transition duration-500 ease-in-out hover:bg-green-500 bg-green-600"
+										type="submit"
+									>
+										Unirme
+									</button>
+									<button
+										onClick={() => closeModal()}
+										className="text-white px-8 py-4 border-none font-bold cursor-pointer rounded-[30px] text-xl uppercase transition duration-500 ease-in-out hover:bg-red-500 bg-red-600"
+										type="submit"
+									>
+										Cancelar
+									</button>
+								</div>
+							</Modal>
+						)}
+
+						{showLeaveModal && (
+							<Modal
+								closeModal={closeModal}
+								isImg={false}
+								title={`Salirse del grupo de ${userData.username}?`}
+							>
+								<div className="gap-4 centered">
+									<button
+										onClick={() => handleLeaveGroup()}
+										className="text-white px-8 py-4 border-none font-bold cursor-pointer rounded-[30px] text-xl uppercase transition duration-500 ease-in-out hover:bg-green-500 bg-green-600"
+										type="submit"
+									>
+										Salirme
+									</button>
+									<button
+										onClick={() => closeModal()}
+										className="text-white px-8 py-4 border-none font-bold cursor-pointer rounded-[30px] text-xl uppercase transition duration-500 ease-in-out hover:bg-red-500 bg-red-600"
+										type="submit"
+									>
+										Cancelar
+									</button>
+								</div>
+							</Modal>
+						)}
+
+						{showDeleteModal && (
+							<Modal
+								closeModal={closeModal}
+								isImg={false}
+								title={`Estas seguro que quiere borrar el grupo?`}
+							>
+								<div className="gap-4 centered">
+									<button
+										onClick={() => handleDeleteGroup()}
+										className="text-white px-8 py-4 border-none font-bold cursor-pointer rounded-[30px] text-xl uppercase transition duration-500 ease-in-out hover:bg-green-500 bg-green-600"
+										type="submit"
+									>
+										Confirmar
+									</button>
+									<button
+										onClick={() => closeModal()}
+										className="text-white px-8 py-4 border-none font-bold cursor-pointer rounded-[30px] text-xl uppercase transition duration-500 ease-in-out hover:bg-red-500 bg-red-600"
+										type="submit"
+									>
+										Cancelar
+									</button>
+								</div>
+							</Modal>
+						)}
+					</>
+				)}
 			</main>
 			<ToastContainer />
 		</>
