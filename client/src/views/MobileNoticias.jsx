1232c46dac9f1ca3e200592ddc908e49d61e735d
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-05-26 19:00:28 +0200

    handleSearch

--- client/src/views/MobileNoticias.jsx
+++ client/src/views/MobileNoticias.jsx
@@ -1,13 +1,64 @@
+import { useState } from "react";
+import { getUsersByNameRoute, userApi } from "../api/APIRoutes";
 import CardUltimosTrades from "../components/CardUltimosTrades";
 import InputSearch from "../components/ui/InputSearch";
+import { Link } from "react-router-dom";
+import Img from "../components/ui/CloudinaryImg";
 
 export default function MobileNoticias() {
+	const [users, setUsers] = useState([]);
+
+	const handleSearch = async (userName) => {
+		try {
+			let data;
+
+			if (userName === "") {
+				data = { users: [] };
+			} else {
+				const response = await userApi.get(
+					`${getUsersByNameRoute}/${userName}`
+				);
+				data = response.data;
+			}
+
+			setUsers(data.users);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
-		<div className="mx-auto max-w-full h-full bg-primario pb-20">
-			<InputSearch />
+		<div className="mx-auto max-w-full h-full bg-white dark:bg-primario pb-20">
+			<InputSearch handleSearch={handleSearch} />
+			{users.length > 0 && (
+				<div className="centered flex-col">
+					{users.slice(0, 3).map((user, index) => {
+						return (
+							<Link
+								className={`contact !flex-row centered !border-white`}
+								key={index}
+								to={`/user/${user.id}`}
+							>
+								{user.image && (
+									<div className="avatar">
+										<Img
+											isContact={true}
+											className="h-14 w-14 avatar-image"
+											uploadedImg={user.image}
+											alt="avatar"
+										/>
+									</div>
+								)}
+								<div className="  username">
+									<h3 className=" text-xl text-white bold">{user.username}</h3>
+								</div>
+							</Link>
+						);
+					})}
+				</div>
+			)}
 			<h2 className="text-center text-2xl font-bold text-white p-4">
 				Últimos trades
-				<span className="text-secundario text-sm ml-1">Ver todos</span>
 			</h2>
 			<div className="flex flex-col justify-center items-center">
 				<CardUltimosTrades />
