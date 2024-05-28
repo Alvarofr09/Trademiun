1caade1fcaf69e18fd1ba1457a54693351569de2
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-05-28 13:46:35 +0200

    Cambiar fotos y descripcion

--- server/utils/dbData.js
+++ server/utils/dbData.js
@@ -6,9 +6,9 @@ const usersData = [
 		email: "alvaro@gmail.com",
 		password: "e10adc3949ba59abbe56e057f20f883e",
 		userRole: false,
-		image: "Trademiun/Users_Avatar/dgzngr3oigc7nvefnzli",
+		image: "Trademiun/Users_Avatar/AlvaroFR_foto_fezhib",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quam blanditiis numquam quo quasi debitis modi omnis nulla voluptatum, eos voluptatibus soluta facere et consequuntur. Magni quasi iure enim sequi!",
+			"Un operador de bolsa experto, siempre analizando los movimientos del mercado en busca de oportunidades lucrativas.",
 		seguidores: 360,
 		rentabilidad: 500,
 	},
@@ -19,29 +19,29 @@ const usersData = [
 		userRole: false,
 		image: "Trademiun/Users_Avatar/dgzngr3oigc7nvefnzli",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quam blanditiis numquam quo quasi debitis modi omnis nulla voluptatum, eos voluptatibus soluta facere et consequuntur. Magni quasi iure enim sequi!",
+			"Una inversora experimentada, hábil en el arte de diversificar su cartera para minimizar riesgos y maximizar ganancias.",
 		seguidores: 367,
 		rentabilidad: 513,
 	},
 	{
-		username: "Pablo",
+		username: "Pablo Fuentes",
 		email: "pablo@gmail.com",
 		password: "e10adc3949ba59abbe56e057f20f883e",
 		userRole: false,
-		image: "Trademiun/Users_Avatar/dgzngr3oigc7nvefnzli",
+		image: "Trademiun/Users_Avatar/PabloFC_foto_ag1kxm",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quam blanditiis numquam quo quasi debitis modi omnis nulla voluptatum, eos voluptatibus soluta facere et consequuntur. Magni quasi iure enim sequi!",
+			"Un trader intrépido, dispuesto a tomar decisiones rápidas y calculadas en medio de la volatilidad del mercado.",
 		seguidores: 143,
 		rentabilidad: 99,
 	},
 	{
-		username: "Parra",
+		username: "Pablo Parra",
 		email: "parra@gmail.com",
 		password: "e10adc3949ba59abbe56e057f20f883e",
 		userRole: false,
-		image: "Trademiun/Users_Avatar/dgzngr3oigc7nvefnzli",
+		image: "Trademiun/Users_Avatar/Pablo_parra_uotmcd",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quam blanditiis numquam quo quasi debitis modi omnis nulla voluptatum, eos voluptatibus soluta facere et consequuntur. Magni quasi iure enim sequi!",
+			"Un especulador astuto, aprovechando las fluctuaciones del mercado para obtener beneficios en corto plazo.",
 		seguidores: 584,
 		rentabilidad: 256,
 	},
@@ -50,31 +50,31 @@ const usersData = [
 		email: "izabel@gmail.com",
 		password: "e10adc3949ba59abbe56e057f20f883e",
 		userRole: false,
-		image: "Trademiun/Users_Avatar/dgzngr3oigc7nvefnzli",
+		image: "Trademiun/Users_Avatar/Izabel_ctcrn4",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quam blanditiis numquam quo quasi debitis modi omnis nulla voluptatum, eos voluptatibus soluta facere et consequuntur. Magni quasi iure enim sequi!",
+			"Una analista técnica meticulosa, utilizando herramientas gráficas y algoritmos para prever tendencias y tomar decisiones informadas.",
 		seguidores: 924,
 		rentabilidad: 712,
 	},
 	{
-		username: "Pedrosa",
+		username: "Pablo Pedrosa",
 		email: "pedrosa@gmail.com",
 		password: "e10adc3949ba59abbe56e057f20f883e",
 		userRole: false,
-		image: "Trademiun/Users_Avatar/dgzngr3oigc7nvefnzli",
+		image: "Trademiun/Users_Avatar/Pablo_Pedrosa_rkz49e",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quam blanditiis numquam quo quasi debitis modi omnis nulla voluptatum, eos voluptatibus soluta facere et consequuntur. Magni quasi iure enim sequi!",
+			"Un gestor de riesgos prudente, siempre evaluando cuidadosamente las posibles pérdidas antes de realizar cualquier operación.",
 		seguidores: 748,
 		rentabilidad: 330,
 	},
 	{
-		username: "Vero",
+		username: "Veronica",
 		email: "vero@gmail.com",
 		password: "e10adc3949ba59abbe56e057f20f883e",
 		userRole: false,
-		image: "Trademiun/Users_Avatar/dgzngr3oigc7nvefnzli",
+		image: "Trademiun/Users_Avatar/vero_nmmwks",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quam blanditiis numquam quo quasi debitis modi omnis nulla voluptatum, eos voluptatibus soluta facere et consequuntur. Magni quasi iure enim sequi!",
+			"Una experta en derivados financieros, aprovechando instrumentos como opciones y futuros para potenciar sus estrategias de inversión.",
 		seguidores: 561,
 		rentabilidad: 19,
 	},
@@ -83,9 +83,9 @@ const usersData = [
 		email: "david@gmail.com",
 		password: "e10adc3949ba59abbe56e057f20f883e",
 		userRole: false,
-		image: "Trademiun/Users_Avatar/dgzngr3oigc7nvefnzli",
+		image: "Trademiun/Users_Avatar/David_nfuccq",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quam blanditiis numquam quo quasi debitis modi omnis nulla voluptatum, eos voluptatibus soluta facere et consequuntur. Magni quasi iure enim sequi!",
+			"Un day trader disciplinado, ejecutando múltiples operaciones en el transcurso de un solo día para capitalizar pequeños movimientos del mercado.",
 		seguidores: 290,
 		rentabilidad: 797,
 	},
@@ -94,20 +94,20 @@ const usersData = [
 		email: "marco@gmail.com",
 		password: "e10adc3949ba59abbe56e057f20f883e",
 		userRole: false,
-		image: "Trademiun/Users_Avatar/dgzngr3oigc7nvefnzli",
+		image: "Trademiun/Users_Avatar/Marco",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quam blanditiis numquam quo quasi debitis modi omnis nulla voluptatum, eos voluptatibus soluta facere et consequuntur. Magni quasi iure enim sequi!",
+			"Un seguidor de tendencias, identificando y aprovechando los movimientos direccionales del mercado para obtener ganancias sostenidas.",
 		seguidores: 643,
 		rentabilidad: 854,
 	},
 	{
-		username: "marina",
-		email: "marina@gmail.com",
+		username: "Nacho",
+		email: "nacho@gmail.com",
 		password: "e10adc3949ba59abbe56e057f20f883e",
 		userRole: false,
-		image: "Trademiun/Users_Avatar/dgzngr3oigc7nvefnzli",
+		image: "Trademiun/Users_Avatar/Nacho",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quam blanditiis numquam quo quasi debitis modi omnis nulla voluptatum, eos voluptatibus soluta facere et consequuntur. Magni quasi iure enim sequi!",
+			"Un inversor a largo plazo, construyendo gradualmente su patrimonio mediante inversiones sólidas y bien fundamentadas en empresas con potencial de crecimiento a largo plazo.",
 		seguidores: 498,
 		rentabilidad: 162,
 	},
@@ -115,73 +115,73 @@ const usersData = [
 
 const groupsData = [
 	{
-		group_name: "Grupo A",
+		group_name: "Trade Marketing",
 		description: "Descripción del Grupo A",
 		price: 150,
-		image: "Trademiun/Groups_Avatar/caoelvgumyzsvs5x0bh5",
+		image: "Trademiun/Groups_Avatar/trade-marketing_fwlhly",
 		admin_id: 1,
 	},
 	{
-		group_name: "Grupo B",
+		group_name: "Autonomo Trade",
 		description: "Descripción del Grupo B",
 		price: 250,
-		image: "Trademiun/Groups_Avatar/caoelvgumyzsvs5x0bh5",
+		image: "Trademiun/Groups_Avatar/autonomo_trade__mazegh",
 		admin_id: 2,
 	},
 	{
-		group_name: "Grupo C",
+		group_name: "Trade.com",
 		description: "Descripción del Grupo C",
 		price: 100,
-		image: "Trademiun/Groups_Avatar/caoelvgumyzsvs5x0bh5",
+		image: "Trademiun/Groups_Avatar/TRADE_COM_atzm7m",
 		admin_id: 3,
 	},
 	{
-		group_name: "Grupo D",
+		group_name: "International Trade",
 		description: "Descripción del Grupo D",
 		price: 300,
-		image: "Trademiun/Groups_Avatar/caoelvgumyzsvs5x0bh5",
+		image: "Trademiun/Groups_Avatar/international-trade_j0nqt3",
 		admin_id: 4,
 	},
 	{
-		group_name: "Grupo E",
+		group_name: "Grupo Trading",
 		description: "Descripción del Grupo E",
 		price: 200,
-		image: "Trademiun/Groups_Avatar/caoelvgumyzsvs5x0bh5",
+		image: "Trademiun/Groups_Avatar/Trading_pyad1r",
 		admin_id: 5,
 	},
 	{
-		group_name: "Grupo F",
+		group_name: "Trade",
 		description: "Descripción del Grupo F",
 		price: 150,
-		image: "Trademiun/Groups_Avatar/caoelvgumyzsvs5x0bh5",
+		image: "Trademiun/Groups_Avatar/Trade_lbpcvr",
 		admin_id: 6,
 	},
 	{
-		group_name: "Grupo G",
+		group_name: "Grupo WDI",
 		description: "Descripción del Grupo G",
 		price: 50,
-		image: "Trademiun/Groups_Avatar/caoelvgumyzsvs5x0bh5",
+		image: "Trademiun/Groups_Avatar/WDI_tgo33f",
 		admin_id: 7,
 	},
 	{
-		group_name: "Grupo H",
+		group_name: "Grupo Agora",
 		description: "Descripción del Grupo H",
 		price: 75,
-		image: "Trademiun/Groups_Avatar/caoelvgumyzsvs5x0bh5",
+		image: "Trademiun/Groups_Avatar/Agora_p20eci",
 		admin_id: 8,
 	},
 	{
-		group_name: "Grupo I",
+		group_name: "Trade Republic",
 		description: "Descripción del Grupo I",
 		price: 200,
-		image: "Trademiun/Groups_Avatar/caoelvgumyzsvs5x0bh5",
+		image: "Trademiun/Groups_Avatar/trade-republic-opiniones_hpbwic",
 		admin_id: 9,
 	},
 	{
-		group_name: "Grupo J",
+		group_name: "Grupo Trade",
 		description: "Descripción del Grupo J",
 		price: 175,
-		image: "Trademiun/Groups_Avatar/caoelvgumyzsvs5x0bh5",
+		image: "Trademiun/Groups_Avatar/Grupo_Trade_mqtfln",
 		admin_id: 10,
 	},
 ];
@@ -226,6 +226,49 @@ const membershipsData = [
 	{
 		group_id: 1,
 		user_id: 10,
+	},
+];
+
+const coinsData = [
+	{
+		name: "Bitcoin",
+		symbol: "BTC",
+	},
+	{
+		name: "Ethereum",
+		symbol: "ETH",
+	},
+	{
+		name: "Ripple",
+		symbol: "XRP",
+	},
+	{
+		name: "Litecoin",
+		symbol: "LTC",
+	},
+	{
+		name: "Cardano",
+		symbol: "ADA",
+	},
+	{
+		name: "Dogecoin",
+		symbol: "DOGE",
+	},
+	{
+		name: "Polkadot",
+		symbol: "DOT",
+	},
+	{
+		name: "Binance Coin",
+		symbol: "BNB",
+	},
+	{
+		name: "Chainlink",
+		symbol: "LINK",
+	},
+	{
+		name: "Stellar",
+		symbol: "XLM",
 	},
 ];
 
@@ -298,4 +341,21 @@ const insertMembership = async () => {
 	}
 };
 
-module.exports = { insertUsers, insertGroups, insertMembership };
+const insertCoins = async () => {
+	let conn = await db.createConection();
+	try {
+		for (const coin of coinsData) {
+			const { name, symbol } = coin;
+			const sqlQuery = `
+        INSERT INTO coins (name, symbol)
+        VALUES (?, ?)
+      `;
+			const params = [name, symbol];
+			await db.query(sqlQuery, params, "insert", conn);
+		}
+	} finally {
+		await conn.end();
+	}
+};
+
+module.exports = { insertUsers, insertGroups, insertMembership, insertCoins };
