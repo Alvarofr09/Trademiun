37ae5bdaf03e88326cddf2bce6b0c9425b58500c
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-05-28 12:24:55 +0200

    TextArea Inutil

--- client/src/components/ui/TextArea.jsx
+++ client/src/components/ui/TextArea.jsx
@@ -3,16 +3,18 @@ import { useField } from "formik";
 export default function TextArea({ props }) {
 	const [field, meta] = useField(props);
 	return (
-		<div className="input-container">
+		<div className="input-container w-full">
 			<textarea
 				{...props}
 				{...field}
-				className={meta.touched && meta.error ? "input-error" : "input"}
-				cols="4"
-				rows="40"
-			>
-				Hola
-			</textarea>
+				className={
+					meta.touched && meta.error
+						? "input-error"
+						: "w-full px-6 text-xl text-primario bg-fondoWebApp bg-opacity-80 border-2 rounded-[30px] border-none border-opacity-50 outline-none focus:border-purple-400  transition duration-200"
+				}
+				// className={`w-full bg-white dark:bg-primario text-primario dark:text-white p-2 rounded-lg focus:border-primario dark:focus:border-white` }
+				rows="4"
+			/>
 		</div>
 	);
 }
